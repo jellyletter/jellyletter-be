ed3cb58df7eb4bdd08df215d99de37e173aed4bf
geesuee <baejisu97@gmail.com>
2024-07-22 20:09:06 +0900

    initial commit

---
+++ src/main/java/com/be/jellyletter/JellyletterApplication.java
@@ -0,0 +1,13 @@
+package com.be.jellyletter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JellyletterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JellyletterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/be/jellyletter/JellyletterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.be.jellyletter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JellyletterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
