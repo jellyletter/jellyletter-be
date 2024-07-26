7c8482397bc434aad561ebef2380c745f68fdf46
geesuee <baejisu97@gmail.com>
2024-07-26 22:22:41 +0900

    feat: 카운슬링 랜덤 반환 시, 반려동물 이름 치환, 조사 치환

--- src/main/java/com/be/jellyletter/controller/CounselingController.java
+++ src/main/java/com/be/jellyletter/controller/CounselingController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,9 +21,9 @@ public class CounselingController {
     private final CounselingService counselingService;
 
     @GetMapping
-    @Operation(summary = "카운슬링 멘트 랜덤 조회 API", description = "DB에 있는 카운슬링 멘트 1개를 랜덤으로 조회하여 반환합니다.")
-    public ResponseEntity<CounselingResDto> getRandomCounseling() {
-        CounselingResDto responseDto = counselingService.getRandomCounseling();
+    @Operation(summary = "카운슬링 멘트 랜덤 조회 API", description = "DB에 있는 카운슬링 멘트 1개를 랜덤으로 조회하여 반환합니다. 반려동물 이름과 이름에 맞는 조사 처리한 텍스트 반환합니다.")
+    public ResponseEntity<CounselingResDto> getRandomCounseling(@RequestParam(name = "petId") Integer petId) {
+        CounselingResDto responseDto = counselingService.getRandomCounselingWithPetName(petId);
 
         return new ResponseEntity<>(responseDto, HttpStatus.OK);
     }
--- src/main/java/com/be/jellyletter/service/CounselingService.java
+++ src/main/java/com/be/jellyletter/service/CounselingService.java
@@ -3,10 +3,15 @@ package com.be.jellyletter.service;
 import com.be.jellyletter.converter.CounselingConverter;
 import com.be.jellyletter.dto.responseDto.CounselingResDto;
 import com.be.jellyletter.model.Counseling;
+import com.be.jellyletter.model.Pet;
 import com.be.jellyletter.repository.CounselingRepository;
+import com.be.jellyletter.repository.PetRepository;
+import jakarta.persistence.criteria.CriteriaBuilder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.NoSuchElementException;
 
 @Service
 @Transactional
@@ -14,10 +19,55 @@ import org.springframework.transaction.annotation.Transactional;
 public class CounselingService {
 
     private final CounselingRepository counselingRepository;
+    private final PetRepository petRepository;
 
-    public CounselingResDto getRandomCounseling() {
+    public CounselingResDto getRandomCounselingWithPetName(Integer petId) {
+        Pet pet = petRepository.findById(petId)
+                .orElseThrow(() -> new NoSuchElementException("Pet with ID " + petId + " not found"));
+
         Counseling counseling = counselingRepository.findRandomCounseling();
+        CounselingResDto dto = CounselingConverter.entityToDto(counseling);
 
-        return CounselingConverter.entityToDto(counseling);
+        String petName = pet.getName();
+
+        // 이름 변경
+        String rawContent = dto.getContent();
+        String contentWithName = replacePetName(rawContent, petName);
+
+        // 조사 매칭
+        String contentWithFitJosa = replaceJosa(contentWithName, petName);
+
+        dto.setContent(contentWithFitJosa);
+
+        return dto;
     }
+
+    private String replacePetName(String content, String petName) {
+        return content.replace("{{반려동물}}", petName);
+    }
+
+    private String replaceJosa(String content, String petName) {
+        char lastChar = petName.charAt(petName.length() -1);
+        String newContent;
+
+        if (hasFinalConsonant(lastChar)) {
+            newContent = content.replace("은/는", "은");
+            newContent = newContent.replace("이/와", "이와");
+            newContent = newContent.replace("이/도", "이도");
+            newContent = newContent.replace("아/야", "아");
+        } else {
+            newContent = content.replace("이/가", "가");
+            newContent = newContent.replace("은/는", "는");
+            newContent = newContent.replace("이/와", "와");
+            newContent = newContent.replace("이/도", "도");
+            newContent = newContent.replace("아/야", "야");
+        }
+
+        return newContent;
+    }
+
+    private static boolean hasFinalConsonant(char c) {
+        return (c - 0xAC00) % 28 > 0;
+    }
+
 }
