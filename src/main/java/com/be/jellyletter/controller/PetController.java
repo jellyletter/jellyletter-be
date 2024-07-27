e42e1f4bb441358315f42a8781158e5eed199b3e
geesuee <baejisu97@gmail.com>
2024-07-27 19:45:21 +0900

    fix: 이미지 프론트 처리, petController 이미지 받는 부분 삭제

--- src/main/java/com/be/jellyletter/controller/PetController.java
+++ src/main/java/com/be/jellyletter/controller/PetController.java
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -26,20 +27,13 @@ import java.io.IOException;
 public class PetController {
 
     private final PetService petService;
-    private final FileService fileService;
 
-    @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
+    @PostMapping
     @Operation(
             summary = "반려동물 생성 API",
-            description = "편지 생성 전 반려동물 정보를 저장합니다. 이미지는 jpeg, png, heic 만 지원합니다."
+            description = "편지 생성 전 반려동물 정보를 저장합니다."
     )
-    public ResponseEntity<PetResDto> createPet(@RequestPart(value = "petReqDto") PetReqDto petReqDto,
-                                               @RequestPart(value = "petImage", required = false) MultipartFile petImage) throws IOException {
-
-        if (petImage != null && !petImage.isEmpty()) {
-            String fileUrl = fileService.uploadFiles(petImage,"pet-image");
-            petReqDto.setImageUrl(fileUrl);
-        }
+    public ResponseEntity<PetResDto> createPet(@Valid @RequestBody PetReqDto petReqDto) throws IOException {
         PetResDto responseDto = petService.createPet(petReqDto);
 
         return new ResponseEntity<>(responseDto, HttpStatus.CREATED);
