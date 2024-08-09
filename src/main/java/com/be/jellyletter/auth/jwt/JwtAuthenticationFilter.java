c3fa4a680f377782df48cc9663719871aa89733c
geesuee <baejisu97@gmail.com>
2024-08-09 17:54:57 +0900

    fix: 토큰 에러 시 401 반환

--- src/main/java/com/be/jellyletter/auth/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/be/jellyletter/auth/jwt/JwtAuthenticationFilter.java
@@ -5,8 +5,10 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.GenericFilterBean;
@@ -21,16 +23,28 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        // Header 에서 JWT 받아오기
-        String token = jwtTokenProvider.resolveToken((HttpServletRequest) servletRequest);
-        System.out.println("***** token: " + token);
 
-        // 토큰 유효성 확인
-        if (token != null && jwtTokenProvider.validateToken(token)) {
-            Authentication authentication = jwtTokenProvider.getAuthentication(token);
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        try {
+            // Header 에서 JWT 받아오기
+            String token = jwtTokenProvider.resolveToken((HttpServletRequest) servletRequest);
+            System.out.println("***** token: " + token);
+
+            // 토큰 유효성 확인
+            if (token != null && jwtTokenProvider.validateToken(token)) {
+                Authentication authentication = jwtTokenProvider.getAuthentication(token);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+
+            filterChain.doFilter(servletRequest, servletResponse);
+        } catch (AuthenticationException e) {
+            // 인증 예외가 발생하면 401 에러를 반환
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, e.getMessage());
+        } catch (Exception e) {
+            // 다른 예외가 발생하면 500 에러를 반환
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "An unexpected error occurred");
         }
-
-        filterChain.doFilter(servletRequest, servletResponse);
     }
 }
--- src/main/java/com/be/jellyletter/config/SecurityConfig.java
+++ src/main/java/com/be/jellyletter/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.be.jellyletter.config;
 
+import com.be.jellyletter.auth.jwt.JwtAuthenticationEntryPoint;
 import com.be.jellyletter.auth.jwt.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -16,55 +17,57 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;  // JwtAuthenticationEntryPoint 추가
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .csrf(csrf -> csrf.disable()) // CSRF 보호 비활성화 (실제 환경에서는 신중히 고려해야 합니다)
-//                .cors(cors -> cors.configurationSource(webConfig.corsConfigurationSource())) // CORS 설정
+                .csrf(csrf -> csrf.disable()) // CSRF 보호 비활성화
+//            .cors(cors -> cors.configurationSource(webConfig.corsConfigurationSource())) // CORS 설정
                 .authorizeHttpRequests(authorize -> authorize
-                .requestMatchers(
-                        "/v3/api-docs/**",
-                        "/swagger-ui/**",
-                        "/swagger-ui.html",
-                        "/swagger-resources/**",
-                        "/webjars/**",
-                        "/swagger-ui/index.html",
-                        "/login/**", // OAuth2 로그인 경로
-                        "/oauth2/**", // OAuth2 경로
-                        "/public/**", // 공개 API 경로
-                        "/static/**", // 정적 자원 경로
-                        "/css/**",
-                        "/js/**",
-                        "/images/**",
-//                        "/api/**",
-                        "/api/letter**",
-                        "/api/letter/pet**",
-                        "/api/pet**",
-                        "/api/file**",
-                        "/api/info**",
-                        "/api/counseling**",
-                        "/api/heat**"
-                ).permitAll()
-                .anyRequest().authenticated()
-            )
-            .oauth2Login(oauth2 -> oauth2 // OAuth2 로그인 설정
-                    .loginPage("/login")
-            )
-            .formLogin(form -> form
-                    .loginPage("/login") // 사용자 정의 로그인 페이지
-                    .permitAll()
-            )
-            .logout(logout -> logout
-                    .permitAll()
-            )
-            .sessionManagement(session -> session
-                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션 사용하지 않음
-            );
+                        .requestMatchers(
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/swagger-resources/**",
+                                "/webjars/**",
+                                "/swagger-ui/index.html",
+                                "/login/**", // OAuth2 로그인 경로
+                                "/oauth2/**", // OAuth2 경로
+                                "/public/**", // 공개 API 경로
+                                "/static/**", // 정적 자원 경로
+                                "/css/**",
+                                "/js/**",
+                                "/images/**",
+                                "/api/letter**",
+                                "/api/letter/pet**",
+                                "/api/pet**",
+                                "/api/file**",
+                                "/api/info**",
+                                "/api/counseling**",
+                                "/api/heat**"
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(oauth2 -> oauth2 // OAuth2 로그인 설정
+                        .loginPage("/login")
+                )
+                .formLogin(form -> form
+                        .loginPage("/login") // 사용자 정의 로그인 페이지
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .permitAll()
+                )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션 사용하지 않음
+                )
+                .exceptionHandling(exception -> exception
+                        .authenticationEntryPoint(jwtAuthenticationEntryPoint)  // JwtAuthenticationEntryPoint 등록
+                );
 
         http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
-
 }
